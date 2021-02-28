ef0e2b4ea5d533b13a3483be612fbe2f042d08d4
Anton Skop-Bilonozhko <ansbi@smart-it.com>
2021-02-28 20:23:25 +0200

    Added first dialogs

--- PolyglotoBot/Controllers/BotController.cs
+++ PolyglotoBot/Controllers/BotController.cs
@@ -1,8 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.11.1
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
@@ -10,9 +5,6 @@ using System.Threading.Tasks;
 
 namespace PolyglotoBot.Controllers
 {
-    // This ASP Controller is created to handle a request. Dependency Injection will provide the Adapter and IBot
-    // implementation at runtime. Multiple different IBot implementations running at different endpoints can be
-    // achieved by specifying a more specific type for the bot constructor argument.
     [Route("api/messages")]
     [ApiController]
     public class BotController : ControllerBase
@@ -29,8 +21,6 @@ namespace PolyglotoBot.Controllers
         [HttpPost, HttpGet]
         public async Task PostAsync()
         {
-            // Delegate the processing of the HTTP POST to the adapter.
-            // The adapter will invoke the bot.
             await Adapter.ProcessAsync(Request, Response, Bot);
         }
     }
---
+++ PolyglotoBot/Models/Configure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolyglotoBot.Models
+{
+    public class Configure
+    {
+        public string WordsCount { get; set; }
+
+        public string RetryCount { get; set; }
+    }
+}
