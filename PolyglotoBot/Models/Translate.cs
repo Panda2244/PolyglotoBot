d44226fc668d0435e6e3b3db718b5addf13cf6e2
Anton Skop-Bilonozhko <ansbi@smart-it.com>
2021-04-04 22:48:10 +0300

    Added TranslateService

---
+++ PolyglotoBot/Models/Translate.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolyglotoBot.Models
+{
+    public class Translate
+    {
+        [JsonProperty("outputs")]
+        public Output[] Outputs { get; set; }
+    }
+
+    public partial class Output
+    {
+        [JsonProperty("output")]
+        public string OutputOutput { get; set; }
+
+        [JsonProperty("stats")]
+        public Stats Stats { get; set; }
+    }
+
+    public partial class Stats
+    {
+        [JsonProperty("elapsed_time")]
+        public long ElapsedTime { get; set; }
+
+        [JsonProperty("nb_characters")]
+        public long NbCharacters { get; set; }
+
+        [JsonProperty("nb_tokens")]
+        public long NbTokens { get; set; }
+
+        [JsonProperty("nb_tus")]
+        public long NbTus { get; set; }
+
+        [JsonProperty("nb_tus_failed")]
+        public long NbTusFailed { get; set; }
+    }
+}
---
+++ PolyglotoBot/Services/TranslateService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using PolyglotoBot.Enums;
+using PolyglotoBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+using System.Threading.Tasks;
+
+namespace PolyglotoBot.Services
+{
+    public class TranslateService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TranslateService(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+        public async Task<Output> GetWordTranslate(string currentWord, Languages currentLanguage,
+        Languages targetLanguage = Languages.en)
+        {
+            var targetLanguageName = Enum.GetName(typeof(Languages), targetLanguage);
+            var currentLanguageNane = Enum.GetName(typeof(Languages), currentLanguage);
+
+            var response = await _httpClient.GetAsync($"/translation/text/translate?" +
+                                                        $"source={currentLanguageNane}" +
+                                                        $"&target={targetLanguageName}" +
+                                                        $"&input={currentWord}");
+
+            response.EnsureSuccessStatusCode();
+
+            var responsejson = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Translate>(responsejson).Outputs.FirstOrDefault();
+        }
+
+    }
+}
