ba5fef276f59e438ff1f88bbbe5c79bb46539385
Anton Skop-Bilonozhko <ansbi@smart-it.com>
2021-04-06 00:05:08 +0300

    added DbContext

---
+++ PolyglotoBot/DB/PolyglotoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolyglotoBot.DB
+{
+    public class PolyglotoDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=TestDatabase.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map table names
+            modelBuilder.Entity<Blog>().ToTable("Blogs", "test");
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.HasKey(e => e.BlogId);
+                entity.HasIndex(e => e.Title).IsUnique();
+                entity.Property(e => e.DateTimeAdd).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
